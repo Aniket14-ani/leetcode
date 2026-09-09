4e722f94281cc592250a1f11f3b740876280b6e9
Aniket Kumar <vinayram503@gmail.com>
2026-09-09 11:13:05 +0530

    Time: 16 ms (92.38%) | Memory: 35 MB (7.51%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        ans=1
+        nums=set(nums)
+        while ans in nums:
+            ans+= 1
+        return ans    
+        
+    
+                
+                 
+
+   
